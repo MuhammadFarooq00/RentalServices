679398b3668ac284a82ebc6876b11eccc53d69b5
Muhammad Farooq <farooqtariq400@gmail.com>
2025-01-30 02:38:10 +0500

    new aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['picsum.photos','via.placeholder.com', 'assets.mixkit.co', 'i.pravatar.cc','images.unsplash.com', 'https://randomuser.me/api/portraits/men/' ,'www.pexels.com','randomuser.me'],
+      domains: ["res.cloudinary.com",'picsum.photos','via.placeholder.com', 'assets.mixkit.co', 'i.pravatar.cc','images.unsplash.com', 'https://randomuser.me/api/portraits/men/' ,'www.pexels.com','randomuser.me'],
     },
     
   }
